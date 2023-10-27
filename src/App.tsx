65ec6d789a33f1dc1c0c5ca46eba8845fa3dc577
Franz <francheizko@gmail.com>
2023-10-27 21:53:13 +0800

    button use state hooks commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,75 @@
-import React from "react"
+import React, { useState } from 'react';
 
 function App() {
+  const [groceryList, setGroceryList] = useState<{ item: string; purchased: boolean }[]>(
+    []
+  ); // Use an array of objects to track both the item and its purchase status
+  const [groceryItem, setGroceryItem] = useState<string>(''); // Provide an explicit type annotation for the state
 
- 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGroceryItem(e.target.value); // Update the groceryItem state when input changes
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (groceryItem.trim() !== '') {
+      setGroceryList([...groceryList, { item: groceryItem, purchased: false }]); // Add the current item to the grocery list with purchased status
+      setGroceryItem(''); // Clear the input field
+    }
+  }
+
+  const handleDelete = (index: number) => {
+    const updatedList = [...groceryList];
+    updatedList.splice(index, 1); // Remove the item at the specified index
+    setGroceryList(updatedList);
+  }
+
+  const handleCheckbox = (index: number) => {
+    const updatedList = [...groceryList];
+    updatedList[index].purchased = !updatedList[index].purchased; // Toggle the purchased status
+    setGroceryList(updatedList);
+  }
+
   return (
     <section className='section-center'>
-    <form className='grocery-form' >
-    
-
-      <h3>grocery bud</h3>
-      <div className='form-control'>
-        <input
-          type='text'
-          className='grocery'
-          placeholder='e.g. eggs'
-        
-        />
-        <button type='submit' className='submit-btn'>
-       
-        </button>
-      </div>
-    </form>
- 
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            value={groceryItem}
+            onChange={handleInputChange}
+          />
+          <button type='submit' className='submit-btn'>
+            Add Item
+          </button>
+        </div>
+      </form>
       <div className='grocery-container'>
-      
-        <button className='clear-btn' >
-          clear items
-        </button>
+        <ul>
+          {groceryList.map((itemData, index) => (
+            <li key={index} className={`grocery-item ${itemData.purchased ? 'purchased' : ''}`}>
+             <div>
+              <input
+                type="checkbox"
+                onChange={() => handleCheckbox(index)}
+                checked={itemData.purchased}
+              />
+              <span className='title'>{itemData.item}</span>
+              </div>
+              <button
+                className='delete-btn'
+                onClick={() => handleDelete(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-    
-  </section>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
